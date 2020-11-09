d9824232869aaa7910c7fe5b55e60393ed2ff780
Lobs <vitor.lobs@gmail.com>
2020-11-08 21:54:31 -0300

    signinup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/project/ProjectDetails';
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 function App() {
   return (
@@ -7,6 +11,12 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <Navbar/>
+          <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          </Switch>
         </div>
       </BrowserRouter>
       
---
+++ src/components/project/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProjectDetails(props) {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <div className="card-title">Project Title {props.match.params.id} </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by the NetNinja</div>
+                    <div>2nd September, 2am</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
