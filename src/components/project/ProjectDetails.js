ae088c81ed30aa4fc4e85aa61d84bde5c91e8cd6
Lobs <vitor.lobs@gmail.com>
2020-11-15 19:20:20 -0300

    project details

--- src/components/project/ProjectDetails.js
+++ src/components/project/ProjectDetails.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
 
 function ProjectDetails(props) {
+    const {project} = props;
+    if (! project){
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+        );
+    }
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <div className="card-title">Project Title {props.match.params.id} </div>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    <span className="card-title"> {project.title} </span>
+                    <p>{project.content}</p>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by the NetNinja</div>
+                    <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
                     <div>2nd September, 2am</div>
                 </div>
             </div>
@@ -17,4 +28,18 @@ function ProjectDetails(props) {
     )
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    const projects  = state.firestore.data.projects;
+    const project = projects ? projects[id] : null;
+    return {
+        project: project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { 'collection':'projects' }
+    ])
+)(ProjectDetails);
